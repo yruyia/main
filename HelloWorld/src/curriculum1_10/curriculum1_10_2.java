4b2c7558cb7da6ec18e7ce99d87486e09fd869ba
zer0 <zer0@LAPTOP-5GLV1DCA>
2023-04-20 09:48:47 +0900

    java1_10

--- HelloWorld/src/curriculum1_10/curriculum1_10_2.java
+++ HelloWorld/src/curriculum1_10/curriculum1_10_2.java
@@ -13,11 +13,16 @@ public class curriculum1_10_2 {
 								・
 						9×1=9 9×2=18…9×9=81
 						*/
+		//i初期値1,iが9以下の時に一を足していく
 		for (int i = 1; i <= 9; i++) {
+			//j初期値1,jが9以下の時に1を足していく
 		    for (int j = 1; j <= 9; j++) {
+		    	//i*jをresultに代入
 		        int result = i * j;
+		        //i * j = resultを表示,タブ文字を挿入
 		        System.out.print(i + "×" + j + "=" + result + "\t");
 		    }
+		    //改行
 		    System.out.println();
 		}
 	}
