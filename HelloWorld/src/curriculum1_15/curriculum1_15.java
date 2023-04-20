a13fed7e799343b8046b539c7ffaddafe967b5d0
zer0 <zer0@LAPTOP-5GLV1DCA>
2023-04-21 08:36:28 +0900

    java1_15

--- HelloWorld/src/curriculum1_15/curriculum1_15.java
+++ HelloWorld/src/curriculum1_15/curriculum1_15.java
@@ -17,22 +17,22 @@ public class curriculum1_15 {
 					  英語の平均点は○○点です。
 		*/
 		// 配列int test[][]に成績を格納
-		int test[][] = { 
+		int test[][] = {
 				{ 64, 73, 69 },
 				{ 58, 81, 75 },
 				{ 86, 68, 79 },
 				{ 72, 55, 80 } };
-		
+
 		// iを0にし、テスト配列より少ないとき1を足し続ける
 		for (int i = 0; i < test.length; i++) {
-			
+
 			// 生徒,教科ごとの成績を表示
 			System.out.println("生徒" + (i + 1) + "：国語" + test[i][0] + "点、数学" + test[i][1] + "点、英語" + test[i][2] + "点");
 		}
-		
+
 		// 改行
 		System.out.println();
-		
+
 		// 教科ごとの合計点数の変数を作成
 		int sumKokugo = 0;
 		int sumSugaku = 0;
@@ -40,11 +40,11 @@ public class curriculum1_15 {
 
 		// iを0にし、テスト配列より少ないとき1を足し続ける
 		for (int i = 0; i < test.length; i++) {
-			
+
 			// 教科ごとに合計点を算出
-		    sumKokugo += test[i][0];
-		    sumSugaku += test[i][1];
-		    sumEigo += test[i][2];
+			sumKokugo += test[i][0];
+			sumSugaku += test[i][1];
+			sumEigo += test[i][2];
 		}
 
 		// intをdoubleに変換し教科ごとの平均点を算出
@@ -56,6 +56,6 @@ public class curriculum1_15 {
 		System.out.println("国語の平均点は" + avgKokugo + "点です。");
 		System.out.println("数学の平均点は" + avgSugaku + "点です。");
 		System.out.println("英語の平均点は" + avgEigo + "点です。");
-		
+
 	}
 }
