16130a0567e7a8c7494a91991e6da1867bd8cd69
zer0 <zer0@LAPTOP-5GLV1DCA>
2023-05-07 08:48:12 +0900

    java22

---
+++ Curriculum22/src/Curriculum22_1/Curriculum22_1.java
@@ -0,0 +1,30 @@
+package Curriculum22_1;
+
+// パッケージ名Curriculum1_22_2のクラス名Curriculum1_22_2をインポート
+import Curriculum22_2.Curriculum22_2;
+
+public class Curriculum22_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		/*
+		下記がコンソールに出力されるように作成してください
+		
+			こんにちは！ここは日本です！
+			この寿司はうまい
+			寿司は和食です
+			今の現在日時は2023/03/09 10:23:39です
+		
+		【条件】
+			・JavaProjectを新規作成し、実行用のpackageと処理用のpackageを作成してください。
+			・適切なファイルにフィールドで変数を必要な数作ってください。
+			・thisを使って作成してください。
+			・日時は今日の日付を取得してください。
+			
+		*/
+
+		// コンストラクタの呼び出し
+		new Curriculum22_2();
+
+	}
+}
---
+++ Curriculum22/src/Curriculum22_2/Curriculum22_2.java
@@ -0,0 +1,50 @@
+package Curriculum22_2;
+
+//　SimpleDateFormatをインポート
+import java.text.SimpleDateFormat;
+// Calendarクラスをインポート
+import java.util.Calendar;
+
+public class Curriculum22_2 {
+
+	// 1行目の変数を生成
+	String aisatu;
+
+	// 2行目の変数を生成
+	String azi;
+
+	// 3行目の変数を生成
+	String shurui;
+
+	// コンストラクターの定義
+	public Curriculum22_2() {
+
+		// 1行目の初期値の設定
+		this.aisatu = "こんにちは！ここは日本です！";
+
+		// 2行目の初期値の設定
+		this.azi = "この寿司はうまい";
+
+		// 3行目の初期値の設定
+		this.shurui = "寿司は和食です";
+
+		// this.aisatuを表示
+		System.out.println(this.aisatu);
+
+		// this.aziを表示
+		System.out.println(this.azi);
+
+		// this.shuruiを表示
+		System.out.println(this.shurui);
+
+		// カレンダーオブジェクトの生成
+		Calendar cl = Calendar.getInstance();
+
+		// SimpleDateFormatクラスでフォーマットパターンを設定する
+		SimpleDateFormat sdf = new SimpleDateFormat("今の現在日時はyyyy/MM/dd H:m:sです");
+
+		// 出力する
+		System.out.println(sdf.format(cl.getTime()));
+
+	}
+}
