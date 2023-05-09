c97a8c2676757192ff8b796278c09b353e811355
zer0 <zer0@LAPTOP-5GLV1DCA>
2023-05-09 17:34:44 +0900

    java25

--- HelloWorld/src/Curriculum1_25/Curriculum1_25.java
+++ HelloWorld/src/Curriculum1_25/Curriculum1_25.java
@@ -3,12 +3,13 @@ package Curriculum1_25;
 // Randomクラスのインポート
 import java.util.Random;
 
-// Curriculum1_25_2のインポート
+// Curriculum1_25_2クラスのインポート
 import Curriculum1_25_2.Curriculum1_25_2;
 
 public class Curriculum1_25 {
 
 	public static void main(String[] args) {
+
 		/*
 		 
 		 名前を入力したら下記がコンソールに出力されるように作ってください
@@ -40,13 +41,13 @@ public class Curriculum1_25 {
 
 		// mpを宣言し0~999までの乱数を代入
 		int mp = random.nextInt(1000);
-		
+
 		// attackを宣言し0~999までの乱数を代入
 		int attack = random.nextInt(1000);
-		
+
 		// agilityを宣言し0~999までの乱数を代入
 		int agility = random.nextInt(1000);
-		
+
 		// defenseを宣言し0~999までの乱数を代入
 		int defense = random.nextInt(1000);
 
@@ -55,25 +56,25 @@ public class Curriculum1_25 {
 
 		// nameを「名前」で初期化
 		String name = "「名前」";
-		
+
 		// コンソールに出力
 		System.out.println("こんにちは " + name + " さん");
-		
+
 		// ゲッターで取得しコンソールに出力
 		System.out.println("HP：" + yusha.getHp());
-		
+
 		// ゲッターで取得しコンソールに出力
 		System.out.println("MP：" + yusha.getMp());
-		
+
 		// ゲッターで取得しコンソールに出力
 		System.out.println("攻撃力：" + yusha.getAttack());
-		
+
 		// ゲッターで取得しコンソールに出力
 		System.out.println("素早さ：" + yusha.getAgility());
-		
+
 		// ゲッターで取得しコンソールに出力
 		System.out.println("防御力：" + yusha.getDefense());
-		
+
 		// コンソールに出力
 		System.out.println("さあ冒険に出かけよう！");
 
--- HelloWorld/src/Curriculum1_25_2/Curriculum1_25_1.java
+++ HelloWorld/src/Curriculum1_25_2/Curriculum1_25_1.java
@@ -4,95 +4,116 @@ public class Curriculum1_25_1 {
 
 	// 変数宣言
 	private int hp;
-	
+
 	// 変数宣言
 	private int mp;
-	
+
 	// 変数宣言
 	private int attack;
-	
+
 	// 変数宣言
 	private int agility;
-	
+
 	// 変数宣言
 	private int defense;
 
 	// コンストラクタの定義
 	public Curriculum1_25_1(int hp, int mp, int attack, int agility, int defense) {
-		
+
 		// コンストラクタが受け取ったhpの値をこのクラスのインスタンス変数であるhpに代入
 		this.hp = hp;
-		
+
 		// コンストラクタが受け取ったmpの値をこのクラスのインスタンス変数であるmpに代入
 		this.mp = mp;
-		
+
 		// コンストラクタが受け取ったattackの値をこのクラスのインスタンス変数であるattackに代入
 		this.attack = attack;
-		
+
 		// コンストラクタが受け取ったagilityの値をこのクラスのインスタンス変数であるagilityに代入
 		this.agility = agility;
-		
+
 		// コンストラクタが受け取ったdefenseの値をこのクラスのインスタンス変数であるdefenseに代入
 		this.defense = defense;
+
 	}
 
 	// getterの定義
 	public int getHp() {
+
 		// hpを返す
 		return hp;
+
 	}
 
 	// setterの定義
 	public void setHp(int hp) {
+
 		// このクラスで定義された「hp」に、引数として渡された値「hp」をセット
 		this.hp = hp;
+
 	}
 
 	// getterの定義
 	public int getMp() {
+
 		// mpを返す
 		return mp;
+
 	}
 
 	// setterの定義
 	public void setMp(int mp) {
+
 		// このクラスで定義された「mp」に、引数として渡された値「mp」をセット
 		this.mp = mp;
+
 	}
 
 	// getterの定義
 	public int getAttack() {
+
 		// attackを返す
 		return attack;
+
 	}
 
 	// setterの定義
 	public void setAttack(int attack) {
+
 		// このクラスで定義された「attack」に、引数として渡された値「attack」をセット
 		this.attack = attack;
+
 	}
 
 	// getterの定義
 	public int getAgility() {
+
 		// agilityを返す
 		return agility;
+
 	}
 
 	// setterの定義
 	public void setAgility(int agility) {
+
 		// このクラスで定義された「agility」に、引数として渡された値「agility」をセット
 		this.agility = agility;
+
 	}
 
 	// getterの定義
 	public int getDefense() {
+
 		// defenseを返す
 		return defense;
+
 	}
 
 	// setterの定義
 	public void setDefense(int defense) {
+
 		// このクラスで定義された「defense」に、引数として渡された値「defense」をセット
 		this.defense = defense;
+
 	}
 }
